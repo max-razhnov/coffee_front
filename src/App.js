390a16ca72731881be28ec673133f5626d3ca5de
max razhnov <shahteryshka@gmail.com>
2019-09-27 23:32:07 +0300

    chore: migrate project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import LoginPage from "./views/LoginPage/index";
+import PageTeamSelect from "./views/PageTeamSelect";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route exact path="/" component={LoginPage} />
+        <Route exact path="/teams" component={PageTeamSelect} />
+      </Router>
     </div>
   );
 }
---
+++ src/views/LoginPage/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+class LoginPage extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <Footer />
+      </>
+    );
+  }
+}
+
+export default LoginPage;
